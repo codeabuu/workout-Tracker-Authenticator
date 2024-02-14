c49e45bbb6feb231a2582a3ea9b624450a49147f
codeabuu <abdulngui718@gmail.com>
2024-02-13 19:08:10 -0500

    sign up, logout hooks

---
+++ frontend/src/hooks/useLogout.js
@@ -0,0 +1,13 @@
+import {useAuthCon} from './useAuth'
+export const useLogout = () => {
+  const{dispatch} = useAuthCon() 
+
+  const logout = () => {
+    //remove user from storage
+    localStorage.removeItem('user')
+    //dispatch logout action
+    dispatch({type: 'LOGOUT'})
+  }
+  return {logout}
+
+}
